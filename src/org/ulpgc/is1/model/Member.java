55585c69bad8711a6fda15e4f89f86e2e2180ce8
Javier <javiergonzavelazquez@gmail.com>
2023-10-18 18:35:13 +0100

    Implementación de Member.

--- src/org/ulpgc/is1/model/Member.java
+++ src/org/ulpgc/is1/model/Member.java
@@ -1,4 +1,13 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+
 public class Member {
+    public ArrayList<Address> address;
+    public int point;
+
+    public void addAddress (String street, int number, int postalCode, String city) {
+        Address a = new Address (street, number, postalCode, city);
+        address.add(a);
+    }
 }
