141f782f00eb397594aa7290e651f49c7a701486
Javier <javiergonzavelazquez@gmail.com>
2023-10-18 13:14:59 +0100

    Implementación de Umpire y método getName.

--- src/org/ulpgc/is1/model/Umpire.java
+++ src/org/ulpgc/is1/model/Umpire.java
@@ -1,4 +1,15 @@
 package org.ulpgc.is1.model;
 
-public class Umpire {
+public class Umpire extends Extra {
+    public String name;
+    public String surname;
+
+    public Umpire (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
