fda2ba7951cd484bd09140fcb174d32e4f0c9df3
Javier <javiergonzavelazquez@gmail.com>
2023-10-19 18:13:28 +0100

    Implementación de isValid() en NIF.

--- src/org/ulpgc/is1/model/NIF.java
+++ src/org/ulpgc/is1/model/NIF.java
@@ -1,4 +1,5 @@
 package org.ulpgc.is1.model;
+import java.util.regex.*;
 
 public class NIF {
     public String number;
@@ -14,4 +15,26 @@ public class NIF {
     public void setNumber(String number) {
         this.number = number;
     }
+
+    public static String isValid(String nif) {
+        if ((nif == null) || (!nif.matches("\\d{8}[A-HJ-NP-TV-Z]"))) {
+            return "XXXX";
+        }
+        String numero = nif.substring(0, 8);
+        char letra = nif.charAt(8);
+        int digitoControlEsperado = calcularDigitoControl(numero);
+        return (digitoControlEsperado == obtenerValorLetra(letra)) ? nif : "XXXX";
+    }
+
+    private static int calcularDigitoControl(String numero) {
+        int num = Integer.parseInt(numero);
+        return num % 23;
+    }
+
+    private static int obtenerValorLetra(char letra) {
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int posicionLetra = letras.indexOf(letra);
+        return posicionLetra % 23;
+    }
+
 }
