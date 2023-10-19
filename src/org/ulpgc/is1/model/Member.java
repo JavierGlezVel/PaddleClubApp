3e75495e5c9e6d566955c90a04cf08016407914e
Javier <javiergonzavelazquez@gmail.com>
2023-10-19 14:05:03 +0100

    Arreglo de Member.

--- src/org/ulpgc/is1/model/Member.java
+++ src/org/ulpgc/is1/model/Member.java
@@ -1,17 +1,12 @@
 package org.ulpgc.is1.model;
 
-import java.util.ArrayList;
-
 public class Member extends Customer {
-    public ArrayList<Address> address;
-    public int point;
+    public Address address;
+    public int points;
 
-    public Member(String name, String surname, NIF nif) {
+    public Member(Address address, int points, String name, String surname, NIF nif) {
         super(name, surname, nif);
-    }
-
-    public void addAddress (String street, int number, int postalCode, String city) {
-        Address a = new Address (street, number, postalCode, city);
-        address.add(a);
+        this.points = points;
+        this.address = address;
     }
 }
