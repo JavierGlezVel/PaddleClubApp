3b3508c919ff69c01a32b71947d82ef8b00418e5
albegnzalezhernandez <alberto.gnzalezherandez@gmail.com>
2023-10-20 11:21:49 +0100

    Finalización del main y principio del PaddleManager.

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -1,23 +1,22 @@
 package org.ulpgc.is1.control;
-
 import org.ulpgc.is1.model.*;
-
+import org.ulpgc.is1.control.PaddleManager;
 public class Main {
     public static void main(String[] args) {
 
         PaddleManager manager = new PaddleManager();
 
-        Customer cliente1 = new Member("Carlos", "Doe", new NIF("45397598K"), 100, new Address("123 Main St.", 20, 2, "35010"));
-        Customer invalidNIFCustomer = new Customer("Alice", "Johnson", new NIF("12SD34TT4"));
+        Customer cliente1 = new Member("Carlos", 100, new NIF("45397598K"), "Almeida", new Address("Aguadulce", 40, 35004, "Las Palmas de Gran Canaria"));
+        Customer cliente2 = new Customer("Pedro", "Domínguez", new NIF("12SD34TT4"));
 
         manager.addCustomer(cliente1);
-        manager.addCustomer(invalidNIFCustomer);
+        manager.addCustomer(cliente2);
 
-        Court fastCourt = new Court("Fast Court 1", 20, CourtType.FAST_COURT);
-        Court slowCourt = new Court("Slow Court 1", 15, CourtType.SLOW_COURT);
+        Court pistaRapida = new Court("Pista rapida 1", 30, CourtType.FastCourt);
+        Court pistaLenta = new Court("Pista lenta 1", 20, CourtType.SlowCourt);
 
-        manager.addCourt(fastCourt);
-        manager.addCourt(slowCourt);
+        manager.addCourt(pistaRapida);
+        manager.addCourt(pistaLenta);
 
         System.out.println(manager.getCustomer(0) + "\n\n");
         System.out.println(manager.getCustomer(1) + "\n\n");
@@ -25,17 +24,17 @@ public class Main {
 
         Reservation reservation = new Reservation(manager.getCustomer(0), manager.getCourt(1));
 
-        reservation.addExtra(new Umpire("Sergio", "Acosta", 100));
+        reservation.addExtra(new Umpire("Alberto", "González Hernández", 150));
 
         manager.reserve(reservation);
 
-        System.out.println("There is " + manager.countCustomer() + " members in the club. \n\n");
+        System.out.println("Existen actualmente " + manager.countCustomer() + " socios en el club. \n\n");
 
         manager.remove(1);
 
-        System.out.println(manager.getCustomer(1) == null ? "Not exist \n\n" : manager.getCustomer(1) + "\n\n");
+        System.out.println(manager.getCustomer(1) == null ? "No existe \n\n" : manager.getCustomer(1) + "\n\n");
 
-        System.out.println("There is " + manager.countCustomer() + " members in the club. \n\n");
+        System.out.println("Existen actualmente" + manager.countCustomer() + " socios en el club. \n\n");
 
         for (Reservation res: manager.getReservations()){
             System.out.println(res);
--- src/org/ulpgc/is1/control/PaddleManager.java
+++ src/org/ulpgc/is1/control/PaddleManager.java
@@ -1,5 +1,29 @@
 package org.ulpgc.is1.control;
 
+import org.ulpgc.is1.model.Court;
+import org.ulpgc.is1.model.Customer;
+import org.ulpgc.is1.model.Reservation;
+
 public class PaddleManager {
 
+    public void addCustomer(Customer cliente1) {
+    }
+
+    public void addCourt(Court pistaRapida) {
+    }
+
+    public boolean getCustomer(int i) {
+    }
+
+    public boolean getCourt(int i) {
+    }
+
+    public void reserve(Reservation reservation) {
+    }
+
+    public String countCustomer() {
+    }
+
+    public Iterable<? extends Reservation> getReservations() {
+    }
 }
